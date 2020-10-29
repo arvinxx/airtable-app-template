1a15f1cea6c4c338fd1b305331e1e358c808ca9b
arvinxx <arvinx@foxmail.com>
2020-10-30 04:10:13 +0800

    :sparkles: feat: add show settings model and settings panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React, { FC } from 'react';
+import { useShowSettings } from './models';
+import { Settings } from './components';
 
 const App: FC = () => {
+  const { isShowSettings } = useShowSettings();
+
   return (
     <div>
       <div>hello world!</div>
+      {isShowSettings && <Settings />}
     </div>
   );
 };
